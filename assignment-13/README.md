3a01535a1613ba58067dabd86dfddb4b71d3f6da
Caitlyn Franke <64992737+CEFranke@users.noreply.github.com>
2022-04-24 03:24:22 -0600

    Added assignment-13 (Final Project Sneak Peek)

---
+++ assignment-13/README.md
@@ -0,0 +1,15 @@
+What is the goal of your website? (Think in terms of your user -- why might they visit your site?)
+
+The technical goal of my site is to be a store. I'm "selling" Dragon Eggs, as you've might have seen from the Egg Emporium from the past. It should have a more fantasy vibe, without hindering a shopping experience. Meaning I need a list of inventory, a form of login system (even if not truly functioning with pure static pages), I need likely a cart of sorts, order page, etc. I want it to be functional, but not too simple looking.
+
+--------------------
+
+Why is responsive web design important?
+
+One of the main benefits of RWD is that it simply makes the user experience easier and better. It can improve readability of the site, or even just make sure a site doesn't break by using it via a different machine (PC vs mobile vs tablet deal). It really just makes design of the page not bothered by extreme resizing and stretching of it.
+Emphasizes “mobile first, with unobtrusive javascript, and progressive enhancement”
+
+--------------------
+
+![Screenshot](./images/indexscreen.PNG)
+![Screenshot](./images/navscreen.PNG)
---
+++ assignment-13/nav.js
@@ -0,0 +1,114 @@
+if (loggedin == null) {
+  var loggedin = false;
+}
+
+if (loggedin == true) {
+document.getElementById("nav-placeholder").innerHTML =
+'<nav>'+
+    '<!-- Creating a NavBar -->'+
+	'<div class="navbar">'+
+
+	   '<div class="content__left lessen3">'+
+           '<a class="active" href="Index.html"><i class="fa fa-caret-home"></i>Home</a>'+
+
+	      '<div class="subnav">'+
+		     '<button class="subnavbtn">Inventory <i class="fa fa-caret-down"></i></button>'+
+
+   	         '<div class="subnav-content">'+
+		           '<a href="egg_list.html"><i class="fa fa-fw fa-database"></i>View all Items</a>'+
+               '<br>'+
+		           '<a href="egg_search.html"><i class="fa fa-fw fa-search"></i>Search Items</a>'+
+	           '</div>'+
+
+   	    '</div>'+
+
+        '<div class="subnav">'+
+		     '<button class="subnavbtn">Information <i class="fa fa-caret-down"></i></button>'+
+
+   	         '<div class="subnav-content">'+
+		         '<a href="about.html"><i class="fa fa-fw fa-database"></i>About</a>'+
+				     '<br>'+
+		         '<a href="dragon.html"><i class="fa fa-fw fa-search"></i>Dragon Types</a>'+
+	         '</div>'+
+
+   	    '</div>'+
+
+      '</div>'+
+
+
+	   '<div class="content__right lessen2">'+
+          '<a href="cart.html"><i class="fa fa-fw fa-shopping-cart"></i>Cart</a>'+
+
+	      '<div class="subnav">'+
+		     '<button class="subnavbtn">Account <i class="fa fa-caret-down"></i></button>'+
+
+   	         '<div class="subnav-content">'+
+		         '<a href="orders.html"><i class="fa fa-fw fa-list"></i>Orders</a>'+
+				 '<br>'+
+		         '<a href="settings.html"><i class="fa fa-fw fa-gear"></i>Settings</a>'+
+	         '</div>'+
+
+   	      '</div>'+
+
+		   '<a href="logout.html"><i class="fa fa-fw fa-sign-out"></i>Logout</a>'+
+	   '</div>'+
+
+    '</div>'+
+'<nav>';
+} else {
+document.getElementById("nav-placeholder").innerHTML =
+'<nav>'+
+    '<!-- Creating a NavBar -->'+
+	'<div class="navbar">'+
+
+    '<div class="content__left">'+
+        '<a class="active sitename" href="Index.html"><i class="fa fa-caret-home"></i>Home</a>'+
+
+        '<div class="lessen1">'+
+        '</div>'+
+
+        '<div class="subnav">'+
+          '<button class="subnavbtn">Inventory <i class="fa fa-caret-down"></i></button>'+
+
+          '<div class="subnav-content">'+
+            '<a href="egg_list.html"><i class="fa fa-fw fa-server"></i>View all Items</a>'+
+            '<br>'+
+            '<a href="egg_list.html"><i class="fa fa-fw fa-search"></i>Search Items</a>'+
+          '</div>'+
+
+        '</div>'+
+
+        '<div class="lessen1">'+
+        '</div>'+
+
+        '<div class="subnav">'+
+          '<button class="subnavbtn">Information <i class="fa fa-caret-down"></i></button>'+
+
+          '<div class="subnav-content">'+
+            '<a href="about.html"><i class="fa fa-fw fa-info-circle"></i>About</a>'+
+            '<br>'+
+            '<a href="dragon.html"><i class="fa fa-fw fa-paw"></i>Dragon Types</a>'+
+          '</div>'+
+
+        '</div>'+
+
+    '</div>'+
+
+
+	   '<div class="content__right">'+
+       '<div class="navback">'+
+   		     '<a href="login.html"><i class="fa fa-fw fa-user"></i>Sign Up</a>'+
+       '</div>'+
+
+       '<div class="lessen2">'+
+       '</div>'+
+
+       '<div class="navback">'+
+		       '<a href="login.html"><i class="fa fa-fw fa-sign-in"></i>Login</a>'+
+       '</div>'+
+	   '</div>'+
+
+    '</div>'+
+'<nav>';
+
+}
